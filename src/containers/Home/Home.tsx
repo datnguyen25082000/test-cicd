c98c6db0722f908892496207816868e5cd376209
Nguyen Tan Dat <datnguyen25082000@gmail.com>
2021-11-27 14:05:19 +0700

    NTDat - gitlab 3

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -62,7 +62,7 @@ export const Home = () => {
 
   return (
     <div className="home">
-      <p>dat qua dep trai</p>
+      <p>dat qua dep trai haha</p>
       <MenuSlideshow />
       <HomeBanner />
       <Menu />
